3238d2e9cbc7fbc0d86ce6e20df365e5d673aaee
Remy Castella <remy.jpen@gmail.com>
2025-02-10 16:49:06 +0900

    allow same letters multiple times

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,7 +7,6 @@ class GamesController < ApplicationController
     @letters = []
     until @letters.length == 10
       @letters << ("a".."z").to_a.sample
-      @letters = @letters.uniq
     end
   end
 
@@ -17,10 +16,10 @@ class GamesController < ApplicationController
     @score = 0
     @message = "Well done!"
     @letters = params[:letters].split("")
-    @word = params[:word]
+    @word = params[:word].downcase
     url = "https://dictionary.lewagon.com/#{@word}"
     @response = JSON.parse(URI.parse(url).read)
-    if @word.downcase.chars.all? { |char| @letters.include?(char) } && @response["found"]
+    if @word.downcase.chars.all? { |char| @letters.count(char) == @word.count(char) } && @response["found"]
       @score += @response["length"]
       session[:total_score] = session[:total_score] + @score
     elsif @response["found"]
